4163ea57605e8bb8469abf7decbd02e66c930e84
jrbistuer <jrbistuer@gmail.com>
2024-05-24 20:08:30 +0200

    firebase auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"testcifo03","appId":"1:263353068362:web:d1f54752bf07d9b3816fb3","storageBucket":"testcifo03.appspot.com","apiKey":"AIzaSyAX4VfJLZf7oaIrI9YF46g5Wlq0WktH-xQ","authDomain":"testcifo03.firebaseapp.com","messagingSenderId":"263353068362","measurementId":"G-R5GS6Y0700"})), provideAuth(() => getAuth())]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutusComponent } from './pages/aboutus/aboutus.component';
 import { EditComponent } from './pages/edit/edit.component';
+import { LoginComponent } from './pages/login/login.component';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['private/home']);
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'aboutus', component: AboutusComponent },
-    { path: 'edit/:id', component: EditComponent },
-    { path: '**/*', redirectTo: 'home' }
+    { path: 'login', component: LoginComponent, ...canActivate(redirectLoggedInToHome) },
+    { path: 'private', ...canActivate(redirectUnauthorizedToLogin), children: [
+        { path: 'home', component: HomeComponent },
+        { path: 'aboutus', component: AboutusComponent },
+        { path: 'edit/:id', component: EditComponent },    
+    ]},
+    { path: '**', redirectTo: 'login' }
 ];
